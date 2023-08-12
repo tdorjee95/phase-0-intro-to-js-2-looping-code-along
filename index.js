b82921979c7e65152367d24c9b8583a5bba5177e
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-08-12 13:43:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+
+function writeCards(names) {
+  const thankYouMessages = [];
+
+
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful birthday gift!`;
+    thankYouMessages.push(message);
+  
+  
+  }
+
+  return thankYouMessages;
+}
+
+const messages = writeCards(names);
+console.log(messages);
